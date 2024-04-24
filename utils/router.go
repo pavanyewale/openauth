3f1ddb19b2669d9101bc00feb7934d91bae70b59
Pavan Yewale <pavanyewale1996@gmail.com>
2024-04-24 22:00:27 +0530

    options requests handled

--- utils/router.go
+++ utils/router.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"net/http"
 	"openauth/utils/logger"
 	"time"
 
@@ -11,27 +12,35 @@ import (
 
 func GetHTTPRouter() *gin.Engine {
 	engine := gin.New()
-	engine.Use(RequestTimeMiddleware())
-	engine.Use(RequestIDMiddleware())
+	engine.Use(RequestTimeMiddleware)
+	engine.Use(RequestIDMiddleware)
+	engine.Use(OptionRequestMiddleware)
 	return engine
 
 }
 
-func RequestTimeMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		start := time.Now()
-		c.Next()
-		field := &logger.Fields{}
-		field.AddField("totalTime", time.Since(start).Milliseconds())
-		logger.Infof(c, fmt.Sprintf("%s %s request end ", c.Request.Method, c.Request.URL.Path), field)
+func OptionRequestMiddleware(c *gin.Context) {
+	if c.Request.Method == "OPTIONS" {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.AbortWithStatus(http.StatusOK)
+		return
 	}
 }
 
-func RequestIDMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// Set the request ID in the context
-		c.Set("requestId", uuid.New().String())
-		// Pass control to the next middleware or route handler
-		c.Next()
-	}
+func RequestTimeMiddleware(c *gin.Context) {
+	start := time.Now()
+	c.Next()
+	field := &logger.Fields{}
+	field.AddField("totalTime", time.Since(start).Milliseconds())
+	logger.Infof(c, fmt.Sprintf("%s %s request end ", c.Request.Method, c.Request.URL.Path), field)
+
+}
+
+func RequestIDMiddleware(c *gin.Context) {
+	// Set the request ID in the context
+	c.Set("requestId", uuid.New().String())
+	// Pass control to the next middleware or route handler
+	c.Next()
 }
